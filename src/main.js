84fe9ce465dd1ad8141d21a0025020361a36c196
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-06 13:43:50 +0200

    Mustache conditionals and v-bind , v-html binding

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  data: {
+    yes: 'Yes it is',
+    no: 'No it is not',
+    falseValue: false,
+    myName: 'Cool Name',
+    someHTML: '<h1 style="color:#41b883; background-­color:#35495e;">Getting to Know Vue.js</h1>'
+  },
+  template: `
+    <div>
+      <div>{{ 1 + 1 }}</div>
+      <div>{{ falseValue === false ? yes : no }}</div>
+      <div>{{ 1 == 2 ? yes : no }}</div>
+      <div>{{ 1 + 1 + 1 > 2 ? yes : no }}</div>
+      <div>{{ 'Getting to Know Vue.js'.toUpperCase() }}</div>
+      <div v-bind:name="myName"></div>
+      <div>{{ someHTML }}</div>
+      <div v-html:name="someHTML"></div>
+    </div>
+  `
 })
