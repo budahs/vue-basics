de49fae659ccf38af5a58b51b4c82d9056c78e29
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-06 13:48:11 +0200

    working with v-show

--- src/main.js
+++ src/main.js
@@ -8,22 +8,22 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   data: {
-    yes: 'Yes it is',
-    no: 'No it is not',
-    falseValue: false,
-    myName: 'Cool Name',
-    someHTML: '<h1 style="color:#41b883; background-­color:#35495e;">Getting to Know Vue.js</h1>'
+    yes: true,
+    no: false,
+    maybe: 0,
+    show: 'yes',
+    dontShow: 'no',
+    yesWord: 'yes'
   },
   template: `
     <div>
-      <div>{{ 1 + 1 }}</div>
-      <div>{{ falseValue === false ? yes : no }}</div>
-      <div>{{ 1 == 2 ? yes : no }}</div>
-      <div>{{ 1 + 1 + 1 > 2 ? yes : no }}</div>
-      <div>{{ 'Getting to Know Vue.js'.toUpperCase() }}</div>
-      <div v-bind:name="myName"></div>
-      <div>{{ someHTML }}</div>
-      <div v-html:name="someHTML"></div>
+      <h1>1:<span v-show="yes">Yes</span></h1>
+      <h1>2:<span v-show="no">No</span></h1>
+      <h1>3:<span v-show="show == 'yes'">Yes!</span></h1>
+      <h1>4:<span v-show="dontShow == 'yes'">No :(</span></h1>
+      <h1>5:<span v-show="show == yesWord">Yes!</span></h1>
+      <h1>6:<span v-show="0 == 1 - 1">Yes!</span></h1>
+      <h1>7:<span v-show="0 == 1 + 1">No :(</span></h1>
     </div>
   `
 })
