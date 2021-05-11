890d398c7dfc653200976488d74eb2b65d6fcb6c
Pablo Garcia <pablo.garcia2chromeburner.com>
2021-05-11 18:20:31 +0200

    Input chain modifiers

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// const axios = require('axios').default
+const axios = require('axios').default
 
 Vue.config.productionTip = false
 
@@ -9,32 +9,32 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   data: {
-    messages: []
+    searchText: '',
+    results: []
+  },
+  methods: {
+    search: function () {
+      axios
+        .get(`https://swapi.dev/api/starships/?search=${this.searchText}`)
+        .then(response => {
+          this.results = response.data
+        })
+    }
   },
   template: `
-    <div>
-        <div v-on:click="messages.push('Outer')">
-            <h4>Outer</h4>
-            <div v-on:click.once.capture="messages.push('Middle')">
-                <h4>Middle</h4>
-                <div v-on:click.stop="messages.push('Inner One')">
-                    <h4>Inner One</h4>
-                </div>
-                <div v-on:click="messages.push('Inner B')">
-                    <h4>Inner B</h4>
-                </div>
-            </div>
-        </div>
-        <p>
-            Last clicked:
-            <ol>
-                <li v-for="message in messages">
-                    {{message}}
-                </li>
-            </ol>
-            </p>
-        <input type="button" v-on:click="messages = []" value="Clear" />
-    </div>
-    `
+      <div>
+      <label>Search:
+        <input
+            type="text"
+            v-model="searchText"
+            v-on:keyup.enter.esc="search" />
+      </label>
+      <h5>Results: <small>{{results.count}}</small></h5>
+      <ul>
+        <li v-for="result in results.results">
+          {{result.name}}
+        </li>
+      </ul>
+      </div>
+      `
 })
-
